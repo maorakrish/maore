8cc6084ad2da63f9c27ab0ad5ff224642d8479dd
maorakrish <maorakrish@gmail.com>
2025-04-09 08:02:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BudgetApp from "./BudgetApp.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BudgetApp />);
